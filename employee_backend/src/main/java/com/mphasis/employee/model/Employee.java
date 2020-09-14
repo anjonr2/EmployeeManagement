b43c072f56cd4f26b0330f33507834a882224c2b
anjon.roy <anjon.roy@LTPBAN292372211.corp.mphasis.com>
2020-09-14 23:32:08 +0530

    First commit by Anjon

---
+++ employee_backend/src/main/java/com/mphasis/employee/model/Employee.java
@@ -0,0 +1,52 @@
+package com.mphasis.employee.model;
+
+public class Employee {
+	
+	private long id;
+	private String firstname;
+	private String lastname;
+	private String emailId;
+	
+	
+	public Employee() {
+		
+	}
+	
+	
+	public Employee(long id, String firstname, String lastname, String emailId) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.emailId = emailId;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+
+}
