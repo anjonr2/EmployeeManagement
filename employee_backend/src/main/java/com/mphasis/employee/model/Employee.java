b19259e862e8486ff96f595b4bd1de5f674e5a6a
anjon.roy <anjon.roy@LTPBAN292372211.corp.mphasis.com>
2020-09-15 00:01:25 +0530

    Repository and custom exception created

--- employee_backend/src/main/java/com/mphasis/employee/model/Employee.java
+++ employee_backend/src/main/java/com/mphasis/employee/model/Employee.java
@@ -1,10 +1,27 @@
 package com.mphasis.employee.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name")
 	private String firstname;
+	
+	@Column(name = "last_name")
 	private String lastname;
+	
+	@Column(name = "email_id")
 	private String emailId;
 	
 	
